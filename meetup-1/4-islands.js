051b7b30e10ccfcc3299569e3239c39e9d6f004e
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-08-03 00:43:15 +0900

    Meetup 1 Challenge 4-islands passes all tests

--- meetup-1/4-islands.js
+++ meetup-1/4-islands.js
@@ -1,5 +1,51 @@
 function countIslands(map) {
-  return 0;
+  let countedIslands = 0;
+
+  const visitedTiles = [];
+  for (let row = 0; row < map.length; ++row) {
+    visitedTiles.push([]);
+
+    const currentRow = map[row];
+    for (let col = 0; col < currentRow.length; ++col) {
+      visitedTiles[row].push(false);
+    }
+  }
+
+  const visitTile = (row, col) => {
+    if (visitedTiles[row][col]) return;
+
+    const visitNeighbours = (row, col) => {
+      if (row < 0 || col < 0) return;
+      if (row >= map.length || col >= map[row].length) return;
+
+      if (!visitedTiles[row][col]) {
+        visitedTiles[row][col] = true;
+        if (map[row][col] === 1) {
+          visitNeighbours(row - 1, col);
+          visitNeighbours(row + 1, col);
+          visitNeighbours(row, col + 1);
+          visitNeighbours(row, col - 1);
+        }
+      }
+    };
+
+    visitedTiles[row][col] = true;
+    if (map[row][col] === 1) {
+      countedIslands++;
+      visitNeighbours(row - 1, col);
+      visitNeighbours(row + 1, col);
+      visitNeighbours(row, col + 1);
+      visitNeighbours(row, col - 1);
+    }
+  };
+  for (let row = 0; row < map.length; ++row) {
+    const currentRow = map[row];
+    for (let col = 0; col < currentRow.length; ++col) {
+      visitTile(row, col);
+    }
+  }
+
+  return countedIslands;
 }
 
 module.exports = countIslands;
