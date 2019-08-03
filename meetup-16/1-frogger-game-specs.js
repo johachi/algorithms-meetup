1a95d4c296659e7a6e24740ed77272dfdfdd7780
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-08-03 13:57:41 +0900

    Linting

--- meetup-16/1-frogger-game-specs.js
+++ meetup-16/1-frogger-game-specs.js
@@ -3,14 +3,14 @@ const frogger = require("./1-frogger-game");
 
 describe("1-frogger-game", () => {
   describe("possible paths", () => {
-    it("should return true for 'XOXOOXXXXOX'", ()=> {
-      expect(frogger("XOXOOXXXXOX")).to.be.true
-    })
-  })
+    it("should return true for 'XOXOOXXXXOX'", () => {
+      expect(frogger("XOXOOXXXXOX")).to.be.true;
+    });
+  });
 
   describe("impossible paths", () => {
-    it("should return true for 'XOOXXOXOXOXOOOOX'", ()=> {
-      expect(frogger("XOOXXOXOXOXOOOOX")).to.be.false
-    })
-  })
-})
+    it("should return false for 'XOOXXOXOXOXOOOOX'", () => {
+      expect(frogger("XOOXXOXOXOXOOOOX")).to.be.false;
+    });
+  });
+});
