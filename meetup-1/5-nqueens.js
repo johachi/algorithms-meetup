ccf59d4dc3b3782237d0354d9d1d78714a5f9624
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-08-03 12:15:56 +0900

    Optimized M01C5-nQueens

--- meetup-1/5-nqueens.js
+++ meetup-1/5-nqueens.js
@@ -6,10 +6,12 @@ function nQueens(numberOfTiles) {
 
   if (numberOfTiles < 5) return numberOfTiles - 1;
 
-  const queensOnBottom = Math.ceil(numberOfTiles / 2);
-  const queensOnTop = Math.floor(numberOfTiles / 2);
+  // const queensOnBottom = Math.ceil(numberOfTiles / 2);
+  // const queensOnTop = Math.floor(numberOfTiles / 2);
 
-  return queensOnBottom + queensOnTop;
+  // return queensOnBottom + queensOnTop;
+
+  return numberOfTiles;
 }
 
 module.exports = nQueens;
