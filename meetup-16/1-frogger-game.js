2218ccf74f317f786d190a75100b27928a4299d8
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-08-03 13:58:18 +0900

    Meetup 16 Challenge 1-frogger-game passes all initial tests

--- meetup-16/1-frogger-game.js
+++ meetup-16/1-frogger-game.js
@@ -1,5 +1,18 @@
-function frogger(str){
-  return false;
+function frogger(str) {
+  let oInRows = 0;
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] === "O") {
+      oInRows++;
+    } else {
+      oInRows = 0;
+    }
+
+    if (oInRows > 2) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = frogger;
