8b0a39c7fa036227fec440de928103cdc76aa717
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-08-03 11:10:13 +0900

    Meetup 1 Challenge 5-nQueens passes all tests

--- meetup-1/5-nqueens.js
+++ meetup-1/5-nqueens.js
@@ -1,5 +1,15 @@
 function nQueens(numberOfTiles) {
-  return 0;
+  if (numberOfTiles < 3)
+    throw new Error(
+      "ERROR! nQueens.js: nQueens() can only be called with argument bigger than 3."
+    );
+
+  if (numberOfTiles < 5) return numberOfTiles - 1;
+
+  const queensOnBottom = Math.ceil(numberOfTiles / 2);
+  const queensOnTop = Math.floor(numberOfTiles / 2);
+
+  return queensOnBottom + queensOnTop;
 }
 
 module.exports = nQueens;
