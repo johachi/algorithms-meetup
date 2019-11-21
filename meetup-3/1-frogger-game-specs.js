f144282f0079ce5964696c49d386fb829f2fb9d1
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-11-21 22:02:36 +0900

    M03C01: Frogger Game

---
+++ meetup-3/1-frogger-game-specs.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+const frogger = require("./1-frogger-game");
+
+describe("Meetup 3: 1-frogger-game", () => {
+  describe("possible paths", () => {
+    it("should return true for 'XOXOOXXXXOX'", () => {
+      expect(frogger("XOXOOXXXXOX")).to.be.true;
+    });
+  });
+
+  describe("impossible paths", () => {
+    it("should return false for 'XOOXXOXOXOXOOOOX'", () => {
+      expect(frogger("XOOXXOXOXOXOOOOX")).to.be.false;
+    });
+  });
+});
---
+++ meetup-3/1-frogger-game.js
@@ -0,0 +1,1 @@
+module.exports = require("../meetup-16/1-frogger-game");
